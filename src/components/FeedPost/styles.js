de063764002bfb7751b0d508a08f06ccacc372f8
Gi Diaz <yandelydf@gmail.com>
2022-09-17 11:12:21 -0400

    Creating Components & adding Flatlist

---
+++ src/components/FeedPost/styles.js
@@ -0,0 +1,83 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  post: {
+    backgroundColor: "#fff",
+    marginVertical: 5,
+  },
+  header: {
+    padding: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    width: "100%",
+  },
+  profileImage: {
+    width: 40,
+    height: 40,
+    borderRadius: 25,
+    marginRight: 10,
+  },
+  name: {
+    fontWeight: "500",
+  },
+  subtitle: {
+    color: "gray",
+  },
+  icon: {
+    marginLeft: "auto",
+  },
+  // Body
+  description: {
+    lineHeight: 20,
+    padding: 10,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+  // Footer
+  footer: {
+    paddingHorizontal: 10,
+  },
+
+  // Stats Row
+  statsRow: {
+    flexDirection: "row",
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    paddingVertical: 10,
+    borderColor: "lightgray",
+  },
+  likeIcon: {
+    width: 20,
+    height: 20,
+    marginRight: 5,
+  },
+  likedBy: {
+    color: "gray",
+  },
+  shares: {
+    color: "gray",
+    marginLeft: "auto",
+  },
+  // Buttons Row
+  buttonsRow: {
+    marginVertical: 10,
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  iconButton: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  iconButtonText: {
+    color: "gray",
+    marginLeft: 5,
+    fontWeight: "500",
+  },
+});
---
+++ src/screens/FeedScreen.js
@@ -0,0 +1,60 @@
+import { ScrollView, FlatList } from "react-native";
+import FeedPost from "../components/FeedPost/index";
+import posts from "../../assets/data/posts.json";
+
+const post1 = {
+  id: "p1",
+  createdAt: "19 m",
+  User: {
+    id: "u1",
+    image:
+      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/zuck.jpeg",
+    name: "Vadim Savin",
+  },
+  description:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+  image: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg",
+  numberOfLikes: 11,
+  numberOfShares: 2,
+};
+
+const post2 = {
+  id: "p2",
+  createdAt: "10 m",
+  User: {
+    id: "u2",
+    image:
+      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png",
+    name: "Elon Musk",
+  },
+  description: "Today we launched another rocket 🚀",
+  numberOfLikes: 11,
+  numberOfShares: 2,
+};
+const post3 = {
+  id: "p2",
+  createdAt: "10 m",
+  User: {
+    id: "u2",
+    image:
+      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/jeff.jpeg",
+    name: "Jeff",
+  },
+  description: "View from my office 😍",
+  image: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/5.jpeg",
+  numberOfLikes: 11,
+  numberOfShares: 2,
+};
+
+const FeedScreen = () => {
+  return (
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <FlatList
+        data={posts}
+        renderItem={({ item }) => <FeedPost post={item} />}
+      />
+    </ScrollView>
+  );
+};
+
+export default FeedScreen;
